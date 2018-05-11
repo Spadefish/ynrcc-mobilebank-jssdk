0b903d527bd90222323cd55106903fa23ef5b295
zhaojin <jiiiiiin@yeah.net>
2018-05-11 09:50:52 +0800

    修复没有对jssdk进行压缩的问题 赵津

--- rollup.config.js
+++ rollup.config.js
@@ -2,14 +2,16 @@ import babel from 'rollup-plugin-babel';
 import babelrc from 'babelrc-rollup';
 import istanbul from 'rollup-plugin-istanbul';
 // import uglify from 'rollup-plugin-uglify';
+// 只是es6语法
+import uglify from 'rollup-plugin-uglify-es';
 
 let pkg = require('./package.json');
 let external = Object.keys(pkg.dependencies);
 // console.log('process.env.NODE_ENV === production', process.env.BUILD)
 let plugins = [
   babel(babelrc()),
-  // (process.env.BUILD === 'production' && uglify()),
-   // uglify()
+  (process.env.BUILD === 'production' && uglify()),
+  // uglify()
 ];
 
 if (process.env.BUILD !== 'production') {
@@ -22,6 +24,9 @@ export default {
   entry: 'lib/index.js',
   plugins: plugins,
   external: external,
+  globals: {
+    base64url: 'base64url'
+  },
   targets: [
     {
       dest: pkg.main,
