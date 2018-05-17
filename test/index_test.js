70e8f754b114cf5242d9277b7f6d189830e9500e
yudandan <root>
2018-05-17 15:39:22 +0800

    yudandan commit base64插件由于外部引入

--- test/index_test.js
+++ test/index_test.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import 'babel-polyfill'
 /* setup.js https://github.com/airbnb/enzyme/blob/master/docs/guides/jsdom.md */
 const { JSDOM } = require('jsdom')
@@ -25,18 +24,6 @@ copyProps(window, global)
 
 import {expect} from 'chai'
 const JSBridge = require('../lib').JSBridge
-//
-// describe('done', function (){
-//   it('done test', function (done){
-//     setTimeout(function (){
-//       done()
-//     }, 1900)
-//   })
-//   it('done test1', function (){
-//     console.log('it里的输出')
-//   })
-//   console.log('it外的输出')
-// })
 
 /* eslint-disable no-console */
 describe('连通性测试', () => {
@@ -71,25 +58,19 @@ describe('连通性测试', () => {
         }
       }).then((bridge) => {
         jsBridge = bridge
-        console.log('关闭窗口')
-        jsBridge.closeWindow = () => {
-          console.log('关闭窗口')
-        }
-        resolve()
+        it('测试正常情况', () => {
+          console.log(jsBridge)
+          jsBridge.closeWindow().then((res)=>{
+            console.log('1111', res);
+            expect(res['ReturnCode']).to.be.equal('000000')
+          })
+        })
       }).catch(err => {
         console.log(JSON.stringify(err))
-        done()
       })
     })
-  it('测试正常情况', (done) => {
-    console.log(jsBridge)
-    jsBridge.closeWindow().then((res)=>{
-      console.log('1111', res);
-      expect(res['ReturnCode']).to.be.equal('000000')
-      done()
-    })
-  })
 })
+
 describe('连通性测试', () => {
 
     let jsBridge = null
@@ -123,11 +104,9 @@ describe('连通性测试', () => {
         }
       }).then((bridge) => {
         jsBridge = bridge
-        console.log(1)
-        it('测试业务返回非000000情况', (done) => {
+        it('测试业务返回非000000情况', () => {
           jsBridge.closeWindow().catch((err)=>{
             expect(err['ReturnCode']).to.be.equal('time_out')
-            done();
           })
         })
       }).catch(err => {
